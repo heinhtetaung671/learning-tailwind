2c9e93569982b56c2b96c1ba1a4a68f92c5f7311
Hein Htet Aung <hhein8358@gmail.com>
2024-06-22 15:27:49 +0630

    font-weigt font-colors font-size

---
+++ README.md
@@ -0,0 +1,1 @@
+# I am learning tailwind css framework
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./target/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
